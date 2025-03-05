23fa82816d1858378ae1e5ef5fa76c7e3e90d5bc
nobuya <nobuyaiwao@gmail.com>
2025-03-06 07:55:45 +0900

    storedCard : payment methods mod #1

--- src/stored-card-component/stored-card-component.js
+++ src/stored-card-component/stored-card-component.js
@@ -62,6 +62,15 @@ document.addEventListener("DOMContentLoaded", async () => {
             if (!config) throw new Error("Failed to load client config");
 
             const paymentMethodsResponse = await fetchPaymentMethods(pmReqConfig);
+            paymentMethodsResponse.storedPaymentMethods.forEach(method => {
+                if (method.supportedRecurringProcessingModels) {
+                    method.supportedRecurringProcessingModels = ["CardOnFile"];
+                }
+                if (method.supportedShopperInteractions) {
+                    method.supportedShopperInteractions = ["Ecommerce"];
+                }
+            });
+
             if (!paymentMethodsResponse) throw new Error("Failed to load payment methods");
 
             // Stored Card configuration
